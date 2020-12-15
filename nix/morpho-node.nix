7773d675de4682120f47946674f484fe42a2a57b
Silvan Mosberger <contact@infinisil.com>
2020-12-15 19:12:16 +0100

    Workaround for cabal trying to build dependencies in nix-shell
    For source-repository-package's, cabal tries to build them on its own, even
    when all dependencies are already provided by Nix. Relevant issues:
    - https://github.com/haskell/cabal/issues/6049
    - https://github.com/input-output-hk/ouroboros-network/pull/645
    - https://github.com/haskell/cabal/issues/5586#issuecomment-479576084
    
    This seems to be a problem even with a cabal that includes
    https://github.com/haskell/cabal/pull/6917 (see
    https://github.com/input-output-hk/haskell.nix/issues/720#issuecomment-745397468
    for how to test a cabal-install 3.4)
    
    The only known workaround is to remove the source-repository-package
    sections from cabal.project, but this should only be done for cabal when
    used from a nix-shell, not from cabal without a nix-shell, and not outside
    the nix-shell.
    
    To make this work smoothly, the script `scripts/nix-setup` can be used,
    which splits the source-repository-package sections into cabal.project.srcs,
    which is then again included from here (to make the Nix setup still work).
    Running the script again undoes it.

--- nix/morpho-node.nix
+++ nix/morpho-node.nix
@@ -2,6 +2,7 @@
 , src
 , haskellCompiler ? "ghc865"
 , profile
+, nixShell
 , ...
 }:
 
@@ -9,6 +10,36 @@ pkgs.haskell-nix.cabalProject {
   inherit src;
   name = "morpho-checkpoint-node";
   compiler-nix-name = haskellCompiler;
+
+  # For source-repository-package's, cabal tries to build them on its own, even
+  # when all dependencies are already provided by Nix. Relevant issues:
+  # - https://github.com/haskell/cabal/issues/6049
+  # - https://github.com/input-output-hk/ouroboros-network/pull/645
+  # - https://github.com/haskell/cabal/issues/5586#issuecomment-479576084
+  #
+  # This seems to be a problem even with a cabal that includes
+  # https://github.com/haskell/cabal/pull/6917 (see
+  # https://github.com/input-output-hk/haskell.nix/issues/720#issuecomment-745397468
+  # for how to test a cabal-install 3.4)
+  #
+  # The only known workaround is to remove the source-repository-package
+  # sections from cabal.project, but this should only be done for cabal when
+  # used from a nix-shell, not from cabal without a nix-shell, and not outside
+  # the nix-shell.
+  #
+  # To make this work smoothly, the script `scripts/nix-setup` can be used,
+  # which splits the source-repository-package sections into cabal.project.srcs,
+  # which is then again included from here (to make the Nix setup still work).
+  # Running the script again undoes it.
+  cabalProject =
+    let
+      exists = builtins.pathExists (../cabal.project.srcs);
+      standard = builtins.readFile (src + "/cabal.project");
+      sources = builtins.readFile (../cabal.project.srcs);
+    in if exists then standard + "\n" + sources
+    else if nixShell then throw "You need to run scripts/nix-setup first"
+    else standard;
+
   pkg-def-extras = [
     (hackage: {
       packages = {
