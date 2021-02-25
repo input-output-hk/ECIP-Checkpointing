5d668497de4bb6f6970d2464e4834213117a9b3d
Silvan Mosberger <contact@infinisil.com>
2021-02-25 21:54:42 +0100

    Simplify Morpho.Node.Features.Node
    Most of the code was unnecessary

--- morpho-checkpoint-node/src/Morpho/Node/Features/Node.hs
+++ morpho-checkpoint-node/src/Morpho/Node/Features/Node.hs
@@ -1,65 +1,16 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Morpho.Node.Features.Node
-  ( run,
-    runConfig,
-    NodeLayer (..),
-    createNodeFeature,
-  )
-where
+module Morpho.Node.Features.Node (run) where
 
 import Cardano.Prelude
 import Cardano.Shell.Lib (CardanoApplication (..), runCardanoApplicationWithFeatures)
-import Cardano.Shell.Types (CardanoFeature (..))
-import Morpho.Config.Logging
 import Morpho.Config.Logging (createLoggingFeature)
 import Morpho.Config.Types
 import Morpho.Node.Run (runNode)
 
-data NodeLayer = NodeLayer
-  { nlRunNode :: forall m. MonadIO m => m ()
-  }
-
-createNodeFeature ::
-  LoggingLayer ->
-  NodeConfiguration ->
-  NodeCLI ->
-  IO (NodeLayer, CardanoFeature)
-createNodeFeature loggingLayer nc nCli = do
-  let nodeLayer =
-        NodeLayer
-          { nlRunNode = liftIO $ runNode loggingLayer nc nCli
-          }
-  let cardanoFeature :: CardanoFeature
-      cardanoFeature =
-        CardanoFeature
-          { featureName = "NodeFeature",
-            featureStart = void $ pure nodeLayer,
-            featureShutdown = pure ()
-          }
-  pure (nodeLayer, cardanoFeature)
-
 run :: NodeCLI -> IO ()
 run cli = do
   nodeConfig <- parseNodeConfiguration $ unConfigPath (configFp cli)
-  runConfig cli nodeConfig
-
-runConfig :: NodeCLI -> NodeConfiguration -> IO ()
-runConfig cli nodeConfig = do
-  (features, nodeLayer) <- initializeAllFeatures nodeConfig cli
-  runCardanoApplicationWithFeatures features (cardanoApplication nodeLayer)
-  where
-    cardanoApplication :: NodeLayer -> CardanoApplication
-    cardanoApplication = CardanoApplication . nlRunNode
-
-initializeAllFeatures ::
-  NodeConfiguration ->
-  NodeCLI ->
-  IO ([CardanoFeature], NodeLayer)
-initializeAllFeatures nodeConfig nCli = do
-  (loggingLayer, loggingFeature) <- createLoggingFeature nCli nodeConfig
-  (nodeLayer, nodeFeature) <- createNodeFeature loggingLayer nodeConfig nCli
-  pure
-    ( [loggingFeature, nodeFeature] :: [CardanoFeature],
-      nodeLayer
-    )
+  (loggingLayer, loggingFeature) <- createLoggingFeature cli nodeConfig
+  runCardanoApplicationWithFeatures [loggingFeature] $
+    CardanoApplication $ runNode loggingLayer nodeConfig cli
