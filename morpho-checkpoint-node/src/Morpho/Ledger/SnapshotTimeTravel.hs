cf6fbac03e502419098fa1d7a0b339a598058604
Silvan Mosberger <contact@infinisil.com>
2021-03-10 17:58:52 +0100

    Remove race condition in getLatestStableLedgerState
    Now possible with the ouroboros bump

--- morpho-checkpoint-node/src/Morpho/Ledger/SnapshotTimeTravel.hs
+++ morpho-checkpoint-node/src/Morpho/Ledger/SnapshotTimeTravel.hs
@@ -25,12 +25,12 @@ data TimeTravelError blk
 
 getLatestStableLedgerState ::
   forall m blk.
-  (MonadIO m, MonadSTM m, LedgerSupportsProtocol blk) =>
+  (MonadSTM m, LedgerSupportsProtocol blk) =>
   ChainDB m blk ->
   Int ->
   m (Either (TimeTravelError blk) (LedgerState blk))
-getLatestStableLedgerState chainDB offset = do
-  anchoredFragment <- atomically (getCurrentChain chainDB)
+getLatestStableLedgerState chainDB offset = atomically $ do
+  anchoredFragment <- getCurrentChain chainDB
   let result = AF.head $ AF.dropNewest offset anchoredFragment
   case result of
     Left _ -> return $ Left $ ChainNotLongEnough offset (AF.length anchoredFragment)
@@ -38,11 +38,7 @@ getLatestStableLedgerState chainDB offset = do
   where
     getLedger hdr = do
       let pnt = blockPoint hdr
-      -- There is a race condition here. If the ledger db advances a lot between
-      -- the two chainDB api calls, the ledger state will not be found and 'Nothing'
-      -- will return. Newer ouroboros-consensus make past ledgerState available
-      -- in STM transactions and can eliminate this race condition.
-      mstate <- atomically $ getPastLedger chainDB (castPoint pnt)
+      mstate <- getPastLedger chainDB (castPoint pnt)
       case ledgerState <$> mstate of
         Nothing -> return $ Left $ LedgerStateNotFoundAt (castPoint pnt)
         Just st -> return $ Right st
