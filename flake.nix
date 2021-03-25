9371ae6046773627be702e505846dcddf92b3a6a
Silvan Mosberger <contact@infinisil.com>
2021-03-25 17:02:10 +0100

    Apply bubblewrap patch for improved wrapped cabal repl experience
    Running Ctrl-C in a cabal repl previously would exit the repl
    completely, instead of just clearing the line. This is because Ctrl-C
    sends a SIGINT to the bubblewrap process, which causes it to quit.
    
    So this commit fixes this by applying a bubblewrap patch that forwards
    SIGINT to the child process, allowing cabal repl to clear the line
    properly on Ctrl-C.
    
    In addition, this commit runs bubblewrap with exec to save a process

--- flake.nix
+++ flake.nix
@@ -75,7 +75,18 @@
           mantis
 
           # Used by scripts/bin/cabal
-          pkgs.bubblewrap
+          (pkgs.bubblewrap.overrideAttrs (old: {
+            patches = old.patches or [] ++ [
+              # https://github.com/containers/bubblewrap/pull/402
+              # Patch for bubblewrap to forward SIGINT (Ctrl-C) to the running
+              # process, allowing Ctrl-C in cabal repl to properly clear the
+              # current line
+              (pkgs.fetchpatch {
+                url = "https://github.com/containers/bubblewrap/pull/402/commits/77bc87e6f9042000a56091539ce2ca66660cd068.patch";
+                sha256 = "08psqg7bkapg9cgipszjs6xh6pcjcg0la6p5rp4abi7il6cyj0fj";
+              })
+            ];
+          }))
         ];
         shellHook = ''
           source scripts/wrap-cabal/wrap-cabal.sh
